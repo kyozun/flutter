13017333b21d7dc834f8f8f8ca9f48c66f5ace0a
kyozun <daicacuong0311@gmail.com>
2024-08-26 12:12:41 +0700

    Sqlite 3

---
+++ lib/database/db_helper.dart
@@ -0,0 +1,51 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper() => _instance;
+
+  DBHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await initDB();
+    return _database!;
+  }
+
+  Future<Database> initDB() async {
+    final path = join(await getDatabasesPath(), 'ecommerce.db');
+    return await openDatabase(
+      path,
+      onCreate: (db, version) {
+        return db.execute('''
+          CREATE TABLE cart(
+            id INTEGER PRIMARY KEY,
+            productId INTEGER,
+            productName TEXT,
+            price REAL,
+            quantity INTEGER
+          )
+        ''');
+      },
+      version: 1,
+    );
+  }
+
+  Future<int> insert(String table, Map<String, dynamic> data) async {
+    final db = await database;
+    return await db.insert(table, data);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAll(String table) async {
+    final db = await database;
+    return await db.query(table);
+  }
+
+  Future<int> delete(String table, int id) async {
+    final db = await database;
+    return await db.delete(table, where: 'id = ?', whereArgs: [id]);
+  }
+}
